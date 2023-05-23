c02cf0bc8ce71f7c2ff076227646fb29de1f34e1
Kevin Rojas <kevinrojasdg@gmail.com>
2023-05-22 22:15:24 -0300

    Making all functions needed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let sueldoTotal, porcentajeSueldoU1, porcentajeSueldoU2;
+
 let app = new Vue ({
     el: '#main',
 
@@ -11,20 +13,44 @@ let app = new Vue ({
 
         sueldoU1: "Sueldo Usuario 1",
         sueldoU2: "Sueldo Usuario 2",
+
+        motivo: "Motivo",
+        valor: "Valor",
+
+        factura: {
+            razon: "",
+            precio: 0
+        },
+        allFacturas: []
     },
 
     methods: {
         calcularPorcentajes: function () {
-            let sueldoTotal = parseFloat(this.sueldoU1) + parseFloat(this.sueldoU2);
-            console.log(sueldoTotal);
+            sueldoTotal = parseFloat(this.sueldoU1) + parseFloat(this.sueldoU2);
+
+            porcentajeSueldoU1 = (parseFloat(this.sueldoU1) * 100) / sueldoTotal;
+
+            porcentajeSueldoU2 = (parseFloat(this.sueldoU2) * 100) / sueldoTotal;
         },
 
         agregarNueva: function () {
             this.modal= true;
+            this.calcularPorcentajes();
+        },
+
+        agregar: function () {
+            this.factura.razon = this.motivo;
+            this.factura.precio = parseFloat(this.valor);
+
+            this.allFacturas.push(this.factura);
+
+            console.log(this.allFacturas);
+            this.factura = [];
+            this.cerrar ();
         },
 
         cerrar: function () {
-            this.model= false;
-        },
+            this.modal= false;
+        }
     }
 });
