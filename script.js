62f8afbe56fc8562f254131a869b25c97c3df737
Kevin Rojas <kevinrojasdg@gmail.com>
2023-05-24 20:54:01 -0300

    First try to add a component

--- script.js
+++ script.js
@@ -2,6 +2,33 @@
 
 let sueldoTotal, porcentajeSueldoU1, porcentajeSueldoU2;
 
+/*
+Vue.component ('sueldos-usuarios', {
+    template: `
+        <form action="">
+            <div>
+                <label for="usuario1">{{usuario1}}</label>
+                <input id="usuario1" type="number" name="usuario1" placeholder="Sueldo Usuario 1" v-model="sueldoU1">
+            </div>
+            
+            <div style="margin-top: 25px;">
+                <label for="usuario2" v-once>{{usuario2}}</label>
+                <input id="usuario2" type="number" name="usuario2" placeholder="Sueldo Usuario 2" v-model="sueldoU2">
+            </div>
+        </form>
+    `,
+
+    props: ['usuario1', 'usuario2'],
+
+    data: function () {
+        return {
+            sueldoU1: "Sueldo Usuario 1",
+            sueldoU2: "Sueldo Usuario 2",
+        }
+    }
+});
+*/
+
 let app = new Vue ({
     el: '#main',
 
@@ -9,10 +36,10 @@ let app = new Vue ({
         usuario1: 'Nombre Usuario 1',
         usuario2: 'Nombre Usuario 2',
 
-        modal: false,
-
         sueldoU1: "Sueldo Usuario 1",
         sueldoU2: "Sueldo Usuario 2",
+
+        modal: false,
 
         motivo: "Motivo",
         valor: "Valor",
@@ -27,7 +54,7 @@ let app = new Vue ({
     },
 
     methods: {
-        calcularPorcentajes: function () {
+        calcularPorcentajes () {
             sueldoTotal = parseFloat(this.sueldoU1) + parseFloat(this.sueldoU2);
 
             porcentajeSueldoU1 = (parseFloat(this.sueldoU1) * 100) / sueldoTotal;
@@ -35,12 +62,12 @@ let app = new Vue ({
             porcentajeSueldoU2 = (parseFloat(this.sueldoU2) * 100) / sueldoTotal;
         },
 
-        agregarNueva: function () {
+        agregarNueva () {
             this.modal= true;
             this.calcularPorcentajes();
         },
 
-        agregar: function () {
+        agregar () {
             this.factura.razon = this.motivo;
             this.factura.precio = parseFloat(this.valor);
             this.factura.precioUsuario1 = (this.factura.precio * porcentajeSueldoU1) / 100;
@@ -53,11 +80,11 @@ let app = new Vue ({
             this.cerrar ();
         },
 
-        cerrar: function () {
+        cerrar () {
             this.modal= false;
         },
 
-        borrar: function () {
+        borrar () {
         }
     }
 });
